0f0b62b58de36b21a15de1ead597e0e0830961e5
HHUUYYLLEE <lebahuybk@gmail.com>
2024-12-23 23:21:50 +0700

    first commit

---
+++ src/constants/toastify.ts
@@ -0,0 +1,8 @@
+export const contextClass = {
+  success: "bg-gradient-to-r from-yellow-300 to-orange-300",
+  error: "bg-gradient-to-r from-yellow-300 to-orange-300",
+  info: "bg-gradient-to-r from-yellow-300 to-orange-300",
+  warning: "bg-gradient-to-r from-yellow-300 to-orange-300",
+  default: "bg-gradient-to-r from-yellow-300 to-orange-300",
+  dark: "bg-gradient-to-r from-yellow-300 to-orange-300",
+};
